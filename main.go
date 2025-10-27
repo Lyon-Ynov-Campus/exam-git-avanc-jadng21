e854f15c839c7b6c00f60f18f388cda6d10f5d72
jadeng21 <jade.ngoma69@gmail.com>
2025-10-27 16:27:13 +0100

    README.md

--- main.go
+++ main.go
@@ -6,9 +6,14 @@ import (
 	"net/http"
 )
 
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
+		http.HandleFunc("/color", ColorHandler)
 	})
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
